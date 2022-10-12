b007388c86ba21c6b036b9f566952fbbf198f07a
Dmitry Shishkov <99988422+DimDimShishkov@users.noreply.github.com>
2022-10-12 19:08:08 +0300

    Delete backend directory

--- backend/src/components/ProtectedRoute.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-
-export const ProtectedRoute = ({ component: Component, ...props }) => {
-  return <Route>{() => (props.loggedIn ? <Component {...props} /> : <Redirect to="/sign-up" />)}</Route>;
-};
